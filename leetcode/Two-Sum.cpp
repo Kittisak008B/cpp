686d6efef5a65e8082c06111ca8f838ef71dc5e1
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-06-06 01:44:44 +0700

    Update Two-Sum.cpp

--- leetcode/Two-Sum.cpp
+++ leetcode/Two-Sum.cpp
@@ -1,30 +1,45 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
-  vector<int> twoSum(vector<int> &nums, int target) {
-    for (int i = 0; i < nums.size(); i++) {
-      for (int j = 0 ; j < nums.size() ; j++){
-
-        if (i !=j && nums[i] + nums[j] == target) {
-                    return {i, j};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int> Hashmap ;
+        for (int i = 0 ; i < nums.size() ; i++){
+            if (Hashmap.count(target - nums[i])) {
+                return {Hashmap[target - nums[i]] , i} ;
+            }
+            Hashmap[nums[i]] = i ;
         }
-      }
+        return {} ;
     }
-    return {};
-  }
 };
 
-// call twoSum with input [3,2,5,14,7] and output 19
-int main() {
-  Solution s;
-  vector<int> nums = {3,2,5,14,7};
-  int target = 19;
-  vector<int> result = s.twoSum(nums, target);
-  for (int i = 0; i < result.size(); i++) {
-    cout << result[i] << endl;
-  }
-  return 0;
-}
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// class Solution {
+// public:
+//   vector<int> twoSum(vector<int> &nums, int target) {
+//     for (int i = 0; i < nums.size(); i++) {
+//       for (int j = 0 ; j < nums.size() ; j++){
+
+//         if (i !=j && nums[i] + nums[j] == target) {
+//                     return {i, j};
+//         }
+//       }
+//     }
+//     return {};
+//   }
+// };
+
+// // call twoSum with input [3,2,5,14,7] and output 19
+// int main() {
+//   Solution s;
+//   vector<int> nums = {3,2,5,14,7};
+//   int target = 19;
+//   vector<int> result = s.twoSum(nums, target);
+//   for (int i = 0; i < result.size(); i++) {
+//     cout << result[i] << endl;
+//   }
+//   return 0;
+// }
